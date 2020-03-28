8eddbda15f596c7022b3f95e0f62f9d53df12fb5
Thiago <toliveirat@icloud.com>
2020-03-28 08:15:44 +0000

    importing withRouter()

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
-  };
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+
+function App(){
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
